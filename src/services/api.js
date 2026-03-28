9c39bf9d59cab16da7a4769e5124631bc6eb695b
Omkar-Suryawanshi <suryawanshio639@mail.com>
2026-03-28 06:08:09 +0530

    save changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://fitness-monolith-u4so.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
